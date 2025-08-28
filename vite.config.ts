4dc3c6e1a6246d66251e4cd31e194f65c4c8a37d
Mats Huesmann <huesmann.mats@gmail.com>
2025-08-29 01:34:52 +0200

    Add more content

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    {enforce: 'pre', ...mdx({providerImportSource: "@mdx-js/react"})},
+    {enforce: 'pre', ...mdx({providerImportSource: "@mdx-js/react", remarkPlugins: [remarkGfm]})},
     react({include: /\.(jsx|js|mdx|md|tsx|ts)$/})
   ],
 })
